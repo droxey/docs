e01ed537d99c3980c7a1e1d410b6f13f0db953ea
Dani Roxberry <dani@bitoriented.com>
2018-08-04 23:59:19 -0700

    [add] dotenv

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,14 +1,9 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 // See https://docusaurus.io/docs/site-config.html for all the possible
 // site configuration options.
 
-/* List of projects/orgs using your project for the users page */
+const dotenv = require("dotenv");
+dotenv.config();
+
 const users = [
   //   {
   //     caption: "me",
